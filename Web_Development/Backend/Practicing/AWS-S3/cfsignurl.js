3a68ef54bc8c24544149fd28a2ae2c3f300c1b35
Md Mushahid Ansari <md.mushahidansari@gmail.com>
2026-02-01 18:09:11 +0530

    practicing cloudfront function for download and getting files

--- Web_Development/Backend/Practicing/AWS-S3/cfsignurl.js
+++ Web_Development/Backend/Practicing/AWS-S3/cfsignurl.js
@@ -1,14 +1,13 @@
 import { getSignedUrl } from "@aws-sdk/cloudfront-signer"; 
 import {readFile} from 'fs/promises'
 
-const cloudfrontDistributionDomain = "https://d1m2dzl351o784.cloudfront.net";
-const s3ObjectKey = "index.html";
-const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}`;
+const cloudfrontDistributionDomain = "https://d24gtp8x4ffd63.cloudfront.net";
+const s3ObjectKey = "6970f9b06392c17c7a494a50.jpg";
+const url = `${cloudfrontDistributionDomain}/${s3ObjectKey}?download=false&filename=mypic.png`;
 const privKey=await readFile('./private_key.pem','utf-8');
-console.log(privKey)
 const privateKey = privKey
-const keyPairId = "KTKYQVC8RF6DT"; // Public key Id 
-const dateLessThan = "2026-01-20";
+const keyPairId = "K1WWF25J0YVIQ7"; // Public key Id 
+const dateLessThan = "2026-02-05";
 
 const signedUrl = getSignedUrl({
   url,
@@ -18,3 +17,4 @@ const signedUrl = getSignedUrl({
 });
 
 console.log(signedUrl)
+
