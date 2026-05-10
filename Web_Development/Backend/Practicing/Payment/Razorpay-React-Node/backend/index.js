eb3b1fc5be5e636c1d41f4548d57e29cb053c849
Md Mushahid Ansari <md.mushahidansari@gmail.com>
2026-05-10 10:41:43 +0530

    practicing webhooks in razorpay

--- Web_Development/Backend/Practicing/Payment/Razorpay-React-Node/backend/index.js
+++ Web_Development/Backend/Practicing/Payment/Razorpay-React-Node/backend/index.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import cors from "cors";
 import Razorpay from "razorpay";
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -27,8 +28,15 @@ app.post("/create-order", async (req, res) => {
   }
 });
 
-
-
+app.post("/razorpay-webhook", async (req, res) => {
+  console.log(req.body,req.body.payload);
+  const providedSignature=req.headers['x-razorpay-signature']
+  console.log(providedSignature)
+  const isSignatureValid=Razorpay.validateWebhookSignature(JSON.stringify(req.body),providedSignature,'mysecret')
+  console.log(isSignatureValid)
+  console.log(req.body.payload.payment.entity.status)
+  res.json({message: "got the event"})
+});
 
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
--- Web_Development/Backend/Practicing/Payment/WorkingWithRazorpaySDK/index.js
+++ Web_Development/Backend/Practicing/Payment/WorkingWithRazorpaySDK/index.js
@@ -1,7 +1,13 @@
 import Razorpay from "razorpay";
 
+const accountId = 'acc_SWeSS1GSMOmsix'; // Your sub-merchant ID
+const webhookId = 'SnVg1gg8n4MdqO';     // The unique webhook ID
 
+instance.webhooks.delete(webhookId,accountId)
+  .then((response) => {
+    console.log("Webhook deleted successfully", response);
+  })
+  .catch((error) => {
+    console.error("Error deleting webhook", error);
+  });
 
-const data= await rzpInstance.orders.all();
-
-console.log(data)
