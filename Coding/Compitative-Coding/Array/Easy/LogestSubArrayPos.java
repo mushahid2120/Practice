f6e31cff7911bab042ee9d6fee5b426f3551c4b6
Md Mushahid Ansari <md.mushahidansari@gmail.com>
2026-09-12 21:13:41 +0530

    Refactor LongestSubArrayPos and RotateLeftByK implementations for improved readability and efficiency

--- Coding/Compitative-Coding/Array/Easy/LogestSubArrayPos.java
+++ Coding/Compitative-Coding/Array/Easy/LogestSubArrayPos.java
@@ -14,32 +14,49 @@
 //  the sum of its elements (5 + 2 + 7 + 1) equals 15. 
 //  Therefore, the length of this sub-array is 4.
 
-import java.util.ArrayList;
-import java.util.TreeSet;
+import java.util.*;
 
 public class LogestSubArrayPos {
     public static void main(String[] args) {
-        int []nums={1,2,3};
-        int k=3,sum=0,start=0,count=0;
-        for(int i=0;i<nums.length;i++){
-            while(sum>k){
-                sum-=nums[start++];
+        int[] nums = {0,0,0,0,0,0,0,0,0,0};
+        int k = 0, sum = 0, count = 0;
+        int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     sum = 0;
+        //     for (int j = i; j < n; j++) {
+        //         sum += nums[j];
+        //         if (sum == k) {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // int left=0,right=0;
+        // sum=nums[0];
+        // while(right<n){
+        //     while(left<=right && sum>k){
+        //         sum-=nums[left++];
+        //     }
+        //     if(sum==k){
+        //         sum-=nums[left++];
+        //         ++count;
+        //     }
+        //     ++right;
+        //     if(right<n){
+        //         sum+=nums[right];
+        //     }
+        // }
+
+        HashMap<Integer,Integer> PrefixSum=new HashMap<>();
+        PrefixSum.put(0,1);
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            int rem=sum-k;
+            if(PrefixSum.containsKey(rem)){
+                count+=PrefixSum.get(rem);
             }
-            while(sum<k){
-                sum+=nums[i++];
-            }
-            if(sum==k){
-                sum-=nums[start++];
-                count++;
-            }
-        }
-        while(sum>k){
-            sum-=nums[start++];
-        }
-        if(sum==k){
-            count++;
+            PrefixSum.put(sum,PrefixSum.getOrDefault(sum, 0)+1);
         }
         System.out.println(count);
-
     }
 }
--- Coding/Compitative-Coding/Array/Easy/RotateLeftByK.java
+++ Coding/Compitative-Coding/Array/Easy/RotateLeftByK.java
@@ -6,58 +6,78 @@
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
 public class RotateLeftByK {
+    public static void reverse(int[] nums, int start, int end) {
+        int n = start+(end - start)/2;
+        System.out.print(start+", "+end+" n="+n+"\n");
+        System.out.println();
+        for (int i = start; i <n; i++) {
+            int temp = nums[i];
+            nums[i] = nums[end - (i-start) - 1];
+            nums[end - (i-start) - 1] = temp;
+
+        }
+        // for(int i=0;i<n;i++){
+        //     System.out.print(nums[i]+", ");
+        // }
+        // System.out.println();
+    }
+
     public static void main(String[] args) {
-        int[]nums={1,2,3,4,5,6,7};
-        int k=3;
-        k=k%nums.length;
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        k = k % nums.length;
         // System.out.println(finalK);
 
-        //Time Complexity - O(N*N)
+        // Time Complexity - O(N*N)
         // for(int i=0;i<finalK;i++){
-        //     lastElement=nums[nums.length-1];
-        //     for(int j=nums.length-1;j>0;j--){
-        //         nums[j]=nums[j-1];
-        //     }
-        //     nums[0]=lastElement;
+        // lastElement=nums[nums.length-1];
+        // for(int j=nums.length-1;j>0;j--){
+        // nums[j]=nums[j-1];
+        // }
+        // nums[0]=lastElement;
         // }
 
         // k=k%nums.length;
         // int []newArr=new int[k];
         // int index=0;
         // for(int i=nums.length-k;i<nums.length;i++){
-        //     newArr[index++]=nums[i];
+        // newArr[index++]=nums[i];
         // }
         // index--;
         // for(int i=nums.length-1;i>=0;i--){
-        //     if(i<k){
-        //         nums[i]=newArr[i];
-        //     }else{
-        //         nums[i]=nums[i-k];
-        //     }
+        // if(i<k){
+        // nums[i]=newArr[i];
+        // }else{
+        // nums[i]=nums[i-k];
+        // }
         // }
 
+        // for(int i=0;i<nums.length/2;i++){
+        // int temp=nums[i];
+        // nums[i]=nums[nums.length-i-1];
+        // nums[nums.length-i-1]=temp;
+        // }
 
-        for(int i=0;i<nums.length/2;i++){
-            int temp=nums[i];
-            nums[i]=nums[nums.length-i-1];
-            nums[nums.length-i-1]=temp;
-        }
+        // for(int i=0;i<k/2;i++){
+        // int temp=nums[i];
+        // nums[i]=nums[k-i-1];
+        // nums[k-i-1]=temp;
+        // }
+        // for(int i=k;i<k+((nums.length-k)/2);i++){
+        // System.out.println(i);
+        // int temp=nums[i];
+        // nums[i]=nums[nums.length+k-i-1];
+        // nums[nums.length+k-i-1]=temp;
+        // }
 
-        for(int i=0;i<k/2;i++){
-            int temp=nums[i];
-            nums[i]=nums[k-i-1];
-            nums[k-i-1]=temp;
-        }
-        for(int i=k;i<k+((nums.length-k)/2);i++){
-            System.out.println(i);
-            int temp=nums[i];
-            nums[i]=nums[nums.length+k-i-1];
-            nums[nums.length+k-i-1]=temp;
-        }
+        int n = nums.length;
 
+        reverse(nums, 0, n);
+        reverse(nums, 0, k);
+        reverse(nums, k, n);
 
-        for(int i:nums){
-            System.out.print(i+", ");
+        for (int i : nums) {
+            System.out.print(i + ", ");
         }
     }
 }
